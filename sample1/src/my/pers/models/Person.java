7f1b149b994b9b0374cb91ed6c0c7ea8d0c35558
sdavid69 <sumit.david69@gmail.com>
2018-07-11 21:00:52 +0530

    Added model

---
+++ sample1/src/my/pers/models/Person.java
@@ -0,0 +1,17 @@
+package my.pers.models;
+public class Person {
+	public String getPersonId() {
+		return personId;
+	}
+	public void setPersonId(String personId) {
+		this.personId = personId;
+	}
+	public String getPersonName() {
+		return personName;
+	}
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+	private String personId;
+	private String personName;
+}
